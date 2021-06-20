8420fb86993d0963806fe5c9ecda421516c52954
ericksitocg <ericksitocg@hotmail.com>
2021-06-20 01:29:36 -0500

    Metodo de trancision "irMenuPrincipal"

--- app/src/main/java/com/example/amst4/MainActivity.java
+++ app/src/main/java/com/example/amst4/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.amst4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void irMenuPrincipal(View v){
+        Intent menuPrincipal = new Intent(getBaseContext(), Menu.class);
+        startActivity(menuPrincipal);
+    }
+
 }
